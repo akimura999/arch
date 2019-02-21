4ca2d2ddacf7549b5cdbc5647f12ebd481650618
akimura999 <47705316+akimura999@users.noreply.github.com>
2019-02-21 15:42:52 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -65,8 +65,7 @@ pacstrap /mnt base base-devel
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt 
-pacman -S git
+arch-chroot /mnt pacman -S git
 git clone git://github.com/akimura999/arch
 cd /arch 
 sh arch2.sh
